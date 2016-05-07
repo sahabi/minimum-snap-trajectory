b776c98fe999612af58443edbfc4cf79445471b2
sahabi <sahabi@gmail.com>
2016-05-06 20:56:19 -0400

    first commit

--- minsnap.cpp
+++ minsnap.cpp
@@ -1,12 +1,9 @@
-// example: construct a quadratic program from given iterators
-// the QP below is the first quadratic program example in the user manual
 #include <iostream>
 #include <CGAL/basic.h>
 #include <CGAL/QP_models.h>
 #include <CGAL/QP_functions.h>
 #include <list>
 #include <math.h>
-// choose exact integral type
 #ifdef CGAL_USE_GMP
 #include <CGAL/Gmpz.h>
 typedef CGAL::Gmpz ET;
@@ -36,59 +33,33 @@ void print(float matrix[t-1][m+1][m+1])
     int i, j, z;
     for (z = 0; z < t-1; z++)
     {
-    for (i = 0; i < m+1; ++i)
-    {
-        for (j = 0; j < m+1; ++j)
-            printf("%f ", matrix[z][i][j]);
-        printf("\n");
-    }
-}
-}
-
-int * concat(int list1[], int list2[]){
-  static int list[sizeof(list1)/sizeof(*list1) + sizeof(list2)/sizeof(*list2)];
-  for (int counter = 0; counter < (sizeof(list)/sizeof(*list)); counter++)
-  {
-    if (counter < sizeof(list1)/sizeof(*list1))
-    {
-      list[counter] = list1[counter];
-    }
-    else
-    {
-      list[counter] = list2[counter-(sizeof(list1)/sizeof(*list1))];
-    }
-
-    
+      for (i = 0; i < m+1; ++i)
+      {
+          for (j = 0; j < m+1; ++j)
+              printf("%f ", matrix[z][i][j]);
+          printf("\n");
+      }
   }
-  return list;
 }
+
+// int * concat(int list1[], int list2[]){
+//   static int list[sizeof(list1)/sizeof(*list1) + sizeof(list2)/sizeof(*list2)];
+//   for (int counter = 0; counter < (sizeof(list)/sizeof(*list)); counter++)
+//   {
+//     if (counter < sizeof(list1)/sizeof(*list1))
+//     {
+//       list[counter] = list1[counter];
+//     }
+//     else
+//     {
+//       list[counter] = list2[counter-(sizeof(list1)/sizeof(*list1))];
+//     }
+//   }
+//   return list;
+// }
 
 int main() {
-  int  Ax[] = {1, -1};                        // column for x
-  int  Ay[] = {1,  2};                        // column for y
-  int*  A[] = {Ax, Ay};                       // A comes columnwise
-  int   b[] = {7, 4};                         // right-hand side
-  CGAL::Const_oneset_iterator<CGAL::Comparison_result> 
-        r(    CGAL::SMALLER);                 // constraints are "<="
-  bool fl[] = {true, true};                   // both x, y are lower-bounded
-  int   l[] = {0, 0};
-  bool fu[] = {false, true};                  // only y is upper-bounded
-  int   u[] = {0, 4};                         // x's u-entry is ignored
-  int  D1[] = {2};                            // 2D_{1,1}
-  int  D2[] = {0, 8};                         // 2D_{2,1}, 2D_{2,2}
-  int*  D[] = {D1, D2};                       // D-entries on/below diagonal
-  int   c[] = {0, -32};
-  int  c0   = 64;                             // constant term
-  // now construct the quadratic program; the first two parameters are
-  // the number of variables and the number of constraints (rows of A)
-  Program qp (2, 2, A, b, r, fl, l, fu, u, D, c, c0);
-  // solve the program, using ET as the exact type
-  Solution s = CGAL::solve_quadratic_program(qp, ET());
-  // output solution
-  std::cout << s;
-  int list1[2] = {0,1};
-  int list2[2] = {2,3};
-
+  
   int C[m+1];
   float pH[t-1][m+1][m+1];
   float nH[t-1][m+1][m+1];
@@ -119,20 +90,40 @@ int main() {
           pH[q][i][z] = 0;
           nH[q][i][z] = 0;
         }
-        H[q][i][z] = pH[q][i][z] - nH[q][i][z];
-        //std::cout << H[q][i][z] <<std::endl; 
-             
+        H[q][i][z] = pH[q][i][z] - nH[q][i][z];             
       }
     }
   }
-  print(pH);
+  // print(pH);
 
-  int *p = concat(list1,list2);
-  int list[sizeof(list1)/sizeof(*list1) + sizeof(list2)/sizeof(*list2)];
-  for (int counter = 0; counter < (sizeof(list)/sizeof(*list)); counter++)
-  {
-    list[counter] = *(p+counter);
-    //std::cout << *(p+counter) <<std::endl;
-  } 
+  // int *p = concat(list1,list2);
+  // int list[sizeof(list1)/sizeof(*list1) + sizeof(list2)/sizeof(*list2)];
+  // for (int counter = 0; counter < (sizeof(list)/sizeof(*list)); counter++)
+  // {
+  //   list[counter] = *(p+counter);
+  //   //std::cout << *(p+counter) <<std::endl;
+  // }
+  int  Ax[] = {1, -1};                        // column for x
+  int  Ay[] = {1,  2};                        // column for y
+  int*  A[] = {Ax, Ay};                       // A comes columnwise
+  int   b[] = {7, 4};                         // right-hand side
+  CGAL::Const_oneset_iterator<CGAL::Comparison_result> 
+        r(    CGAL::SMALLER);                 // constraints are "<="
+  bool fl[] = {true, true};                   // both x, y are lower-bounded
+  int   l[] = {0, 0};
+  bool fu[] = {false, true};                  // only y is upper-bounded
+  int   u[] = {0, 4};                         // x's u-entry is ignored
+  int  D1[] = {2};                            // 2D_{1,1}
+  int  D2[] = {0, 8};                         // 2D_{2,1}, 2D_{2,2}
+  int*  D[] = {D1, D2};                       // D-entries on/below diagonal
+  int   c[] = {0, -32};
+  int  c0   = 64;                             // constant term
+  // now construct the quadratic program; the first two parameters are
+  // the number of variables and the number of constraints (rows of A)
+  Program qp (2, 2, A, b, r, fl, l, fu, u, D, c, c0);
+  // solve the program, using ET as the exact type
+  Solution s = CGAL::solve_quadratic_program(qp, ET());
+  // output solution
+  std::cout << s;
   return 0;
 }
